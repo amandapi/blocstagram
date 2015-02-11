4985c3fc9bf7d0e36dd2b7dc876405fc29bd9680
Amanda Pi <amandapi88@gmail.com>
2015-02-11 13:15:19 -0800

    Initial Commit

---
+++ Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Amanda Pi on 2015-02-11.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Blocstagram/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Blocstagram
+//
+//  Created by Amanda Pi on 2015-02-11.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
